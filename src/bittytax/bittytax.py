f9c8a3cddf6134ad82a8ad5d89c042406fb63eac
Hodlemporor <49566730+hodlemperor@users.noreply.github.com>
2024-10-12 09:49:49 +0200

    ok

--- src/bittytax/bittytax.py
+++ src/bittytax/bittytax.py
@@ -343,7 +343,7 @@ def _do_each_tax_year(
         }
 
         if not summary_only:
-            tax.calculate_yearly_holdings(value_asset,calc_cgt_total,tax_year)
+            tax.calculate_yearly_holdings(value_asset,total_gain_with_margin,tax_year)
             tax.check_holding_threshold(value_asset, tax_year)
             tax.calculate_daily_holdings_and_average(value_asset, tax_year)
 
@@ -373,7 +373,7 @@ def _do_each_tax_year(
 
         if not summary_only:
             tax.calculate_holdings(value_asset)
-            tax.calculate_yearly_holdings(value_asset,calc_cgt_total)
+            tax.calculate_yearly_holdings(value_asset,total_gain_with_margin)
             for year in sorted(tax.tax_events):
                 if year in CCG.CG_DATA_INDIVIDUAL:
                     tax.check_holding_threshold(value_asset, year)
